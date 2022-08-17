0a6131b16ad69818d83259f624a5f8a8738d30fe
Angelina <ajhul394@learner.dvusd.org>
2022-08-17 13:09:12 -0700

    Wednesday changes

---
+++ VB/VideoGames/VideoGames/Form1.Designer.vb
@@ -0,0 +1,314 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UntilDawnButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Namelabel = New System.Windows.Forms.Label()
+        Me.genrelabel = New System.Windows.Forms.Label()
+        Me.ReleasedLabel = New System.Windows.Forms.Label()
+        Me.DesignerLabel = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Uncharted = New System.Windows.Forms.Button()
+        Me.TLOUbutton = New System.Windows.Forms.Button()
+        Me.DBHbutton = New System.Windows.Forms.Button()
+        Me.Quarrybutton = New System.Windows.Forms.Button()
+        Me.thequarrypicturebox = New System.Windows.Forms.PictureBox()
+        Me.DBHpicture = New System.Windows.Forms.PictureBox()
+        Me.TLOUpicturebox = New System.Windows.Forms.PictureBox()
+        Me.PictureBoxuncharted = New System.Windows.Forms.PictureBox()
+        Me.UntilDawnpicturebox = New System.Windows.Forms.PictureBox()
+        Me.displayPictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.thequarrypicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DBHpicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TLOUpicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxuncharted, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UntilDawnpicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'UntilDawnButton
+        '
+        Me.UntilDawnButton.Location = New System.Drawing.Point(52, 187)
+        Me.UntilDawnButton.Name = "UntilDawnButton"
+        Me.UntilDawnButton.Size = New System.Drawing.Size(75, 23)
+        Me.UntilDawnButton.TabIndex = 0
+        Me.UntilDawnButton.Text = "UntilDawn"
+        Me.UntilDawnButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(310, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(310, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Genre:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(310, 112)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Released in:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(310, 148)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(74, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Developer:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(363, 36)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 16)
+        Me.Label5.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(363, 74)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 16)
+        Me.Label6.TabIndex = 8
+        '
+        'Namelabel
+        '
+        Me.Namelabel.AutoSize = True
+        Me.Namelabel.Location = New System.Drawing.Point(363, 36)
+        Me.Namelabel.Name = "Namelabel"
+        Me.Namelabel.Size = New System.Drawing.Size(0, 16)
+        Me.Namelabel.TabIndex = 9
+        '
+        'genrelabel
+        '
+        Me.genrelabel.AutoSize = True
+        Me.genrelabel.Location = New System.Drawing.Point(363, 74)
+        Me.genrelabel.Name = "genrelabel"
+        Me.genrelabel.Size = New System.Drawing.Size(0, 16)
+        Me.genrelabel.TabIndex = 10
+        '
+        'ReleasedLabel
+        '
+        Me.ReleasedLabel.AutoSize = True
+        Me.ReleasedLabel.Location = New System.Drawing.Point(399, 112)
+        Me.ReleasedLabel.Name = "ReleasedLabel"
+        Me.ReleasedLabel.Size = New System.Drawing.Size(0, 16)
+        Me.ReleasedLabel.TabIndex = 11
+        '
+        'DesignerLabel
+        '
+        Me.DesignerLabel.AutoSize = True
+        Me.DesignerLabel.Location = New System.Drawing.Point(381, 148)
+        Me.DesignerLabel.Name = "DesignerLabel"
+        Me.DesignerLabel.Size = New System.Drawing.Size(0, 16)
+        Me.DesignerLabel.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label7.Location = New System.Drawing.Point(203, 190)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(2, 18)
+        Me.Label7.TabIndex = 13
+        '
+        'Uncharted
+        '
+        Me.Uncharted.Location = New System.Drawing.Point(190, 185)
+        Me.Uncharted.Name = "Uncharted"
+        Me.Uncharted.Size = New System.Drawing.Size(99, 23)
+        Me.Uncharted.TabIndex = 15
+        Me.Uncharted.Text = "Uncharted"
+        Me.Uncharted.UseVisualStyleBackColor = True
+        '
+        'TLOUbutton
+        '
+        Me.TLOUbutton.Location = New System.Drawing.Point(318, 187)
+        Me.TLOUbutton.Name = "TLOUbutton"
+        Me.TLOUbutton.Size = New System.Drawing.Size(117, 23)
+        Me.TLOUbutton.TabIndex = 16
+        Me.TLOUbutton.Text = "TheLastofUs1"
+        Me.TLOUbutton.UseVisualStyleBackColor = True
+        '
+        'DBHbutton
+        '
+        Me.DBHbutton.Location = New System.Drawing.Point(460, 187)
+        Me.DBHbutton.Name = "DBHbutton"
+        Me.DBHbutton.Size = New System.Drawing.Size(156, 23)
+        Me.DBHbutton.TabIndex = 17
+        Me.DBHbutton.Text = "DetroitBecomeHuman"
+        Me.DBHbutton.UseVisualStyleBackColor = True
+        '
+        'Quarrybutton
+        '
+        Me.Quarrybutton.Location = New System.Drawing.Point(637, 190)
+        Me.Quarrybutton.Name = "Quarrybutton"
+        Me.Quarrybutton.Size = New System.Drawing.Size(98, 23)
+        Me.Quarrybutton.TabIndex = 18
+        Me.Quarrybutton.Text = "TheQuarry"
+        Me.Quarrybutton.UseVisualStyleBackColor = True
+        '
+        'thequarrypicturebox
+        '
+        Me.thequarrypicturebox.Image = Global.VideoGames.My.Resources.Resources.download__1_
+        Me.thequarrypicturebox.Location = New System.Drawing.Point(637, 225)
+        Me.thequarrypicturebox.Name = "thequarrypicturebox"
+        Me.thequarrypicturebox.Size = New System.Drawing.Size(100, 50)
+        Me.thequarrypicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.thequarrypicturebox.TabIndex = 21
+        Me.thequarrypicturebox.TabStop = False
+        '
+        'DBHpicture
+        '
+        Me.DBHpicture.Image = Global.VideoGames.My.Resources.Resources.download
+        Me.DBHpicture.Location = New System.Drawing.Point(486, 225)
+        Me.DBHpicture.Name = "DBHpicture"
+        Me.DBHpicture.Size = New System.Drawing.Size(100, 50)
+        Me.DBHpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.DBHpicture.TabIndex = 20
+        Me.DBHpicture.TabStop = False
+        '
+        'TLOUpicturebox
+        '
+        Me.TLOUpicturebox.Image = Global.VideoGames.My.Resources.Resources.co1r7f
+        Me.TLOUpicturebox.Location = New System.Drawing.Point(335, 225)
+        Me.TLOUpicturebox.Name = "TLOUpicturebox"
+        Me.TLOUpicturebox.Size = New System.Drawing.Size(100, 50)
+        Me.TLOUpicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.TLOUpicturebox.TabIndex = 19
+        Me.TLOUpicturebox.TabStop = False
+        '
+        'PictureBoxuncharted
+        '
+        Me.PictureBoxuncharted.Image = Global.VideoGames.My.Resources.Resources.Uncharted_4_box_artwork
+        Me.PictureBoxuncharted.Location = New System.Drawing.Point(189, 225)
+        Me.PictureBoxuncharted.Name = "PictureBoxuncharted"
+        Me.PictureBoxuncharted.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBoxuncharted.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxuncharted.TabIndex = 14
+        Me.PictureBoxuncharted.TabStop = False
+        '
+        'UntilDawnpicturebox
+        '
+        Me.UntilDawnpicturebox.Image = Global.VideoGames.My.Resources.Resources.maxresdefault_1_700x3941
+        Me.UntilDawnpicturebox.Location = New System.Drawing.Point(41, 225)
+        Me.UntilDawnpicturebox.Name = "UntilDawnpicturebox"
+        Me.UntilDawnpicturebox.Size = New System.Drawing.Size(100, 50)
+        Me.UntilDawnpicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.UntilDawnpicturebox.TabIndex = 5
+        Me.UntilDawnpicturebox.TabStop = False
+        '
+        'displayPictureBox
+        '
+        Me.displayPictureBox.Location = New System.Drawing.Point(25, 36)
+        Me.displayPictureBox.Name = "displayPictureBox"
+        Me.displayPictureBox.Size = New System.Drawing.Size(264, 128)
+        Me.displayPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.displayPictureBox.TabIndex = 4
+        Me.displayPictureBox.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.thequarrypicturebox)
+        Me.Controls.Add(Me.DBHpicture)
+        Me.Controls.Add(Me.TLOUpicturebox)
+        Me.Controls.Add(Me.Quarrybutton)
+        Me.Controls.Add(Me.DBHbutton)
+        Me.Controls.Add(Me.TLOUbutton)
+        Me.Controls.Add(Me.Uncharted)
+        Me.Controls.Add(Me.PictureBoxuncharted)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.DesignerLabel)
+        Me.Controls.Add(Me.ReleasedLabel)
+        Me.Controls.Add(Me.genrelabel)
+        Me.Controls.Add(Me.Namelabel)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.UntilDawnpicturebox)
+        Me.Controls.Add(Me.displayPictureBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.UntilDawnButton)
+        Me.Name = "Form1"
+        Me.Text = "Video Games "
+        CType(Me.thequarrypicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DBHpicture, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TLOUpicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxuncharted, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UntilDawnpicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UntilDawnButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents displayPictureBox As PictureBox
+    Friend WithEvents UntilDawnpicturebox As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Namelabel As Label
+    Friend WithEvents genrelabel As Label
+    Friend WithEvents ReleasedLabel As Label
+    Friend WithEvents DesignerLabel As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PictureBoxuncharted As PictureBox
+    Friend WithEvents Uncharted As Button
+    Friend WithEvents TLOUbutton As Button
+    Friend WithEvents DBHbutton As Button
+    Friend WithEvents Quarrybutton As Button
+    Friend WithEvents TLOUpicturebox As PictureBox
+    Friend WithEvents DBHpicture As PictureBox
+    Friend WithEvents thequarrypicturebox As PictureBox
+End Class
---
+++ VB/VideoGames/VideoGames/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Private Sub UntilDawnButton_Click(sender As Object, e As EventArgs) Handles UntilDawnButton.Click
+        displayPictureBox.Image = UntilDawnpicturebox.Image
+        Namelabel.Text = "Until Dawn"
+        genrelabel.Text = "Horror"
+        ReleasedLabel.Text = "2015"
+        DesignerLabel.Text = "Supermassive Games"
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Namelabel.Click
+        Namelabel.Text = "Until Dawn"
+        genrelabel.Text = "Horror"
+        ReleasedLabel.Text = "2015"
+        DesignerLabel.Text = "Supermassive Games"
+    End Sub
+
+    Private Sub Label7_Click_1(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Uncharted_Click(sender As Object, e As EventArgs) Handles Uncharted.Click
+        displayPictureBox.Image = PictureBoxuncharted.Image
+        Namelabel.Text = "Uncharted 4"
+        genrelabel.Text = "Action/Adventure"
+        ReleasedLabel.Text = "2016"
+        DesignerLabel.Text = "Naughty Dog"
+
+    End Sub
+
+    Private Sub TLOUbutton_Click(sender As Object, e As EventArgs) Handles TLOUbutton.Click
+        displayPictureBox.Image = TLOUpicturebox.Image
+        Namelabel.Text = "The Last of Us"
+        genrelabel.Text = "Surival Horror/ Action"
+        ReleasedLabel.Text = "2011"
+        DesignerLabel.Text = "Naughty Dog"
+    End Sub
+
+    Private Sub DBHbutton_Click(sender As Object, e As EventArgs) Handles DBHbutton.Click
+        displayPictureBox.Image = DBHpicture.Image
+        Namelabel.Text = "Detroit Become Human"
+        genrelabel.Text = "Action/ Adventure"
+        ReleasedLabel.Text = "2018"
+        DesignerLabel.Text = "Quantic Dream"
+    End Sub
+
+    Private Sub Quarrybutton_Click(sender As Object, e As EventArgs) Handles Quarrybutton.Click
+        displayPictureBox.Image = thequarrypicturebox.Image
+        Namelabel.Text = "The Quarry"
+        genrelabel.Text = "Horror"
+        ReleasedLabel.Text = "2022"
+        DesignerLabel.Text = "Supermassive Games"
+    End Sub
+End Class
