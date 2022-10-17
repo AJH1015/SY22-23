6bfcd12799cd6e661073844e310ff1b2896e71e8
Angelina <ajhul394@learner.dvusd.org>
2022-10-17 12:21:47 -0700

    looping

---
+++ VB/Looping/Looping/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Player = New System.Windows.Forms.PictureBox()
+        Me.Badguy1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Badguy1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Player
+        '
+        Me.Player.Image = CType(resources.GetObject("Player.Image"), System.Drawing.Image)
+        Me.Player.Location = New System.Drawing.Point(173, 100)
+        Me.Player.Name = "Player"
+        Me.Player.Size = New System.Drawing.Size(133, 62)
+        Me.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Player.TabIndex = 0
+        Me.Player.TabStop = False
+        '
+        'Badguy1
+        '
+        Me.Badguy1.Image = Global.Looping.My.Resources.Resources.download__2_
+        Me.Badguy1.Location = New System.Drawing.Point(338, 129)
+        Me.Badguy1.Name = "Badguy1"
+        Me.Badguy1.Size = New System.Drawing.Size(100, 47)
+        Me.Badguy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Badguy1.TabIndex = 1
+        Me.Badguy1.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(374, 79)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 422)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Badguy1)
+        Me.Controls.Add(Me.Player)
+        Me.Font = New System.Drawing.Font("MingLiU_HKSCS-ExtB", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Badguy1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Player As PictureBox
+    Friend WithEvents Badguy1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+End Class
